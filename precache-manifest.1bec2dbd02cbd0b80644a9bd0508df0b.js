552b7b2e010038107854c7a4e5660ae6eae1f84f
Prasad Annadata <prasad@annadata.net>
2023-10-14 20:59:17 +0000

    Updates

---
+++ precache-manifest.1bec2dbd02cbd0b80644a9bd0508df0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224e95e40f70ba62e6472c3d819eada7",
+    "url": "/RummyCount.com/index.html"
+  },
+  {
+    "revision": "a8834a9eb3a7c4372558",
+    "url": "/RummyCount.com/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/RummyCount.com/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/RummyCount.com/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "a8834a9eb3a7c4372558",
+    "url": "/RummyCount.com/static/js/main.db4cc57d.chunk.js"
+  },
+  {
+    "revision": "c7c16fa918e89eb2c817",
+    "url": "/RummyCount.com/static/js/runtime~main.e4c1adc1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/RummyCount.com/static/media/logo.06e73328.svg"
+  }
+]);
